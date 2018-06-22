e9e9bf32223bbc4e387d2daf1601f002c66a082d
Nicholas <39197099+dropino@users.noreply.github.com>
2018-06-22 16:14:53 +0200

    score manager (wip) + shipmanager

---
+++ 2D Multiplayer Space Shooter/Assets/Scripts/ScenesManagers/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    private int _scorePlayer1;
+    private int _scorePlayer2;
+    private int _scorePlayer3;
+    private int _scorePlayer4;
+    private int _maxKills;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- 2D Multiplayer Space Shooter/Assets/Scripts/Ships/Handlers/ShipLifeHandler.cs
+++ 2D Multiplayer Space Shooter/Assets/Scripts/Ships/Handlers/ShipLifeHandler.cs
@@ -5,66 +5,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipLifeHandler : MonoBehaviour 
+public class ShipLifeHandler : MonoBehaviour
 {
-
     //in physics 2D Invulnerable layer is set to not collide with anything
-	private static readonly int INVULN_LAYER = 12;
-	private static readonly float INVULN_TIMER = 0.5f;
+    private static readonly int INVULN_LAYER = 12;
+    private static readonly float INVULN_TIMER = 0.5f;
 
-	[SerializeField]
-	private int _correctLayer;
-	private float _invulnTimer = 0;
-	private bool _isDead = false;
-	private float _health;
-   
-	private Animator _deathAnimation;
+    [SerializeField]
+    private int _correctLayer;
+    private float _invulnTimer = 0;
+    private bool _isDead = false;
+    private float _health;
+
+    private Ship _owner;
+
+    private Animator _deathAnimation;
+
+    private ScoreManager _scoremanager; //per notify Death
 
 
-	public float Health
+    private void Start()
     {
-		get { return this._health; }
-		set { this._health = value; }
+        _correctLayer = gameObject.layer;
+        _owner = gameObject.GetComponent<Ship>();
+
+        _deathAnimation = GetComponent<Animator>();
     }
-   
-
-	private void Start()
-	{
-		_correctLayer = gameObject.layer;
-
-		_deathAnimation = GetComponent<Animator>();
-        
-	}
 
 
-	private void Update()
-	{
-		if (Health <= 0) Die(); //_deathAnimation.SetBool("ShipIsDead", true);
+    private void Update()
+    {
+        if (Health <= 0) Die(); //_deathAnimation.SetBool("ShipIsDead", true);
 
-		if (this.gameObject.layer == INVULN_LAYER)
-		{
-			_invulnTimer -= Time.deltaTime;
+        if (this.gameObject.layer == INVULN_LAYER)
+        {
+            _invulnTimer -= Time.deltaTime;
 
-			if (_invulnTimer <= 0) gameObject.layer = this._correctLayer;
-		}
-	}
+            if (_invulnTimer <= 0) gameObject.layer = this._correctLayer;
+        }
+    }
+
+    public void Die()
+    {
+        _isDead = true;
+        gameObject.SetActive(false);
+    }
+
+    //metodi su modello di dominio:
+
+    public float Health
+    {
+        get { return this._health; }
+        set { this._health = value; }
+    }
 
 
     public void takeDamage(float damageTaken)
     {
-		Health -= damageTaken;
+        Health -= damageTaken;
         gameObject.layer = INVULN_LAYER;
         _invulnTimer = INVULN_TIMER;
-  
+
     }
 
+    public void notifyDeath()
+    {
+        //dice allo score manager che è morto e di aggiornare lo score
+    }
 
-	public void Die() 
-	{
-		_isDead = true;
-		gameObject.SetActive(false);
+    public Boolean GetIsDead()
+    {
+        return _isDead;
+    }
 
-	}
-
-   
 }
